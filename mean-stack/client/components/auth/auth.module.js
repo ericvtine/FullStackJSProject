ab7dfc610c072f70b96a884ee12cc068366ad8b7
Eric Valentine <evalentine89@icloud.com>
2016-07-22 18:18:59 -0400

    Without further ado, my final commit.

--- mean-stack/client/components/auth/auth.module.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-angular.module('meanStackApp.auth', ['meanStackApp.constants', 'meanStackApp.util', 'ngCookies',
-    'ui.router'
-  ])
-  .config(function($httpProvider) {
-    $httpProvider.interceptors.push('authInterceptor');
-  });
